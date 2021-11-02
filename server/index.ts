5c96b2db3d93f20554bb4281e7447fb198c83736
Ben Stephens <BKStephens@outlook.com>
2021-11-02 09:22:55 -0700

    Setup instructions to create .env

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
-const express = require('express');
+import express from 'express';
+import * as dotenv from 'dotenv';
 
 const start = async () => {
+  dotenv.config();
   const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
   const app = express();
 
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
   });
 
   app.listen(port);
-}
+};
 
 start();
